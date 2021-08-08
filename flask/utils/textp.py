191b565011c0e739bebacc80ab18b2d2cc43ac20
Jyotishna Baishya <jyotishna.b800@gmail.com>
2021-08-08 23:28:13 +0530

    gitignore

--- flask/utils/textp.py
+++ flask/utils/textp.py
@@ -21,17 +21,3 @@ def keywords(t):
     words=word_tokenize(t)
     print(words)
     return words
-
-# def sim(word1, word2):
-#     max_sim=0
-#     for net1 in wn.synsets(word1):
-#         for net2 in wn.synsets(word2):
-#             try:
-#                 lch = net1.lch_similarity(net2)
-#             except:
-#                 continue
-#             # The value to compare the LCH to was found empirically.
-#             # (The value is very application dependent. Experiment!)
-#             if lch >= lch_threshold:
-#                 max_sim=max(max_sim,lch)
-#     return max_sim
